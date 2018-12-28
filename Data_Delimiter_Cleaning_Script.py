cece4fba5134784d16b2b42295cc0bddd2fa7b27
Mark Johnson <mrk.jhsn@gmail.com>
2018-12-28 13:52:25 -0700

    Update Data_Delimiter_Cleaning_Script.py

--- Data_Delimiter_Cleaning_Script.py
+++ Data_Delimiter_Cleaning_Script.py
@@ -11,7 +11,7 @@ for row in inputfile:
         headers = re.sub(r',','|',row)
         output_data.write(headers)
     else:   #all other rows need to be cleaned
-        bus_name = row[0:(re.search(r'[,]\d{2,7}[,]', row).start())]  #pulls the business name off the front of the string, using the business number(between 2 and 7 digits) as the point of reference for the regex
+        bus_name = row[0:(re.search(r'[,]\d{2,7}[,]', row).start())]  #pulls the business name off the front of the string, using the business number(between 2 and 7 digits) with commas on either side as the point of reference for the regex
         bus_info_raw = row[(re.search(r'[,]\d{2,7}[,]', row).start()):] #pulls everything after the business name into a string
         bus_info_clean = re.sub(r',','|',bus_info_raw) #does a find/replace to remove all commas from business info string and replace them with bar character
         output_data.write(bus_name+bus_info_clean) #write cleaned data to txt file
